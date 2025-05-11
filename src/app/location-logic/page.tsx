bb5a26a1953963e86ec5d61be87b28f50fa96304
highrow623 <danieltmetro@gmail.com>
2025-05-11 16:21:45 -0600

    get rid of currently unused pages

--- src/app/location-logic/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function LocationLogic() {
-  return (
-    <div>
-      <h1>Location Logic</h1>
-    </div>
-  );
-}
--- src/app/region-logic/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function RegionLogic() {
-  return (
-    <div>
-      <h1>Region Logic</h1>
-    </div>
-  );
-}
