19e78a96f00e6221d3dcd585692c954f51c1c8a9
highrow623 <danieltmetro@gmail.com>
2025-05-11 11:21:18 -0600

    add actual pathing data

--- src/app/pathing-logic/page.tsx
+++ src/app/pathing-logic/page.tsx
@@ -10,14 +10,21 @@ import ClickWrapper from "../components/clickWrapper";
 import { useItems, hasRequiredItems } from "../context/items";
 
 export default function PathingLogic() {
-  const { difficulty, obscure, cycleDifficulty, toggleObscure } = useDifficulty();
+  const { difficulty, obscure, cycleDifficulty, toggleObscure } =
+    useDifficulty();
   const { items } = useItems();
 
   return (
     <div className="flex flex-col items-stretch p-8">
-      <p className="text-slate-200 text-3xl mb-5">Pathing Logic</p>
+      <p className="text-slate-200 text-3xl mb-2">Pathing Logic</p>
+      <p className="text-slate-400 text-xl mb-5">apworld version: 0.8.0</p>
+      <p className="text-slate-300 mb-2">
+        This table shows which locations will be in logic based on your player
+        options and current items.
+      </p>
+      <br />
       <table className="table-auto border-separate border-spacing-1">
-        <thead>
+        <thead className="sticky top-0">
           <tr>
             <th className="text-slate-300 border border-gray-600 p-2 bg-slate-900 rounded-xs text-start">
               <span className="select-none">Location</span>
@@ -25,7 +32,7 @@ export default function PathingLogic() {
             <th className="text-slate-300 border border-gray-600 p-2 bg-slate-900 rounded-xs">
               <div className="flex justify-start">
                 <ClickWrapper onClick={cycleDifficulty} enabled>
-                  <DifficultyIcon difficulty={difficulty}/>
+                  <DifficultyIcon difficulty={difficulty} />
                 </ClickWrapper>
                 <p className="select-none ml-1">Logic Level</p>
               </div>
@@ -33,14 +40,14 @@ export default function PathingLogic() {
             <th className="text-slate-300 border border-gray-600 p-2 bg-slate-900 rounded-xs">
               <div className="flex justify-start">
                 <ClickWrapper onClick={toggleObscure} enabled>
-                  <ObscureIcon obscure={obscure}/>
+                  <ObscureIcon obscure={obscure} />
                 </ClickWrapper>
                 <p className="select-none ml-1">Obscure?</p>
               </div>
             </th>
             <th className="text-slate-300 border border-gray-600 p-2 bg-slate-900 rounded-xs">
               <div className="flex justify-start">
-                <ItemIcons items={items} inHeader/>
+                <ItemIcons items={items} inHeader />
                 <p className="select-none ml-1">Items</p>
               </div>
             </th>
@@ -50,7 +57,16 @@ export default function PathingLogic() {
           </tr>
         </thead>
         <tbody>
-          {rowData.filter(data => data.difficulty <= difficulty && (obscure || !data.obscure) && hasRequiredItems(items, data.items)).map(data => <TableRow key={data.id} rowData={data}/>)}
+          {rowData
+            .filter(
+              (data) =>
+                data.difficulty <= difficulty &&
+                (obscure || !data.obscure) &&
+                hasRequiredItems(items, data.items),
+            )
+            .map((data) => (
+              <TableRow key={data.id} rowData={data} />
+            ))}
         </tbody>
       </table>
     </div>
